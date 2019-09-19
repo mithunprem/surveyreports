41bfc5064c5f50bc3d616a3387734913d11c238d
Mithun Prem <mithunprem19@gmail.com>
2019-09-19 18:55:07 +1000

    Wired the SurveyMetaData component with withRouter and added code to do a page redirect when the user clicks on ‘View Survey Details’ button.

--- src/App/PageComponents/SurveyMetaData.js
+++ src/App/PageComponents/SurveyMetaData.js
@@ -1,30 +1,49 @@
 import React, { Component, Fragment } from 'react';
-import {
-  Col, Card, CardHeader, CardBody, CardText, Button
-} from 'reactstrap';
+import { Col, Card, CardHeader, CardBody, CardText, Button } from 'reactstrap';
+import { withRouter } from 'react-router-dom';
+import { SURVEY_DETAILS_URL } from '../Constants';
 
-export default class SurveyMetaData extends Component {
+class SurveyMetaData extends Component {
+
+  openSurveyDetailsPage = () => {
+    const { history, survey } = this.props;
+
+    history.push({
+      pathname: SURVEY_DETAILS_URL,
+      state: { url: survey.url }
+    });
+  }
 
   render() {
     const {
-      name, participant_count, url,
-      submitted_response_count, response_rate
+      name, submitted_response_count,
+      participant_count, response_rate
     } = this.props.survey;
 
     return (
       <Fragment>
-          <Col xs={12} sm="auto" className="p-3">
-            <Card>
-              <CardHeader tag="h5">{name}</CardHeader>
-              <CardBody>
-                <CardText>Participant Count : {participant_count}</CardText>
-                <CardText>Submitted Response count : {submitted_response_count}</CardText>
-                <CardText>Response Rate : {response_rate.toFixed(2)}%</CardText>
-                <Button>View Survey Details</Button>
-              </CardBody>
-            </Card>
-          </Col>
+        <Col xs={12} sm="auto" className="p-3">
+          <Card>
+            <CardHeader tag="h5">{name}</CardHeader>
+            <CardBody>
+              <CardText>
+                Participant Count : {participant_count}
+              </CardText>
+              <CardText>
+                Submitted Response count : {submitted_response_count}
+              </CardText>
+              <CardText>
+                Response Rate : {(response_rate*100).toFixed(2)}%
+              </CardText>
+              <Button onClick={this.openSurveyDetailsPage}>
+                View Survey Details
+              </Button>
+            </CardBody>
+          </Card>
+        </Col>
       </Fragment>
     )
   }
 }
+
+export default withRouter(SurveyMetaData);
