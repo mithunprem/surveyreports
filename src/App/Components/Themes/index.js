fe45f8bad089e0d6bd3684cad54c9f6af85c7e7b
Mithun Prem <mithunprem19@gmail.com>
2019-09-30 17:06:13 +1000

    Refactored class name in Themes component.

--- src/App/Components/Themes/index.js
+++ src/App/Components/Themes/index.js
@@ -8,7 +8,7 @@ const Themes = ({ themes }) => {
     <Fragment>
       <hr />
       <h6 className="ml-4 mb-2"> Themes Focussed :</h6>
-      <Row className="theme-details ml-1 mb-3 mr-1">
+      <Row className="themes ml-1 mb-3 mr-1">
         {
           themes.map((theme, index) => {
             return <ThemeDetails key={index} theme={theme}/>
